5453ce9751e3343a523435bad13a776dbc6a6b27
Vithor Félix <vithorfjm@outlook.com>
2024-09-09 17:56:23 -0300

    Arquivos iniciais

---
+++ src/main/java/ucb/CaresyncApp/CaresyncAppApplication.java
@@ -0,0 +1,13 @@
+package ucb.CaresyncApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaresyncAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaresyncAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucb/CaresyncApp/CaresyncAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ucb.CaresyncApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaresyncAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
